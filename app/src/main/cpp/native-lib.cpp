dc28cb95a22f0f51a43ebbe1e753041de8e7b4d7
monatis <yusufsarigoz@gmail.com>
2017-09-12 14:07:19 +0300

    Add main and nested detector functionality

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,12 +3,23 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
+
+CascadeClassifier mainClassifier;
+CascadeClassifier nestedClassifier;
+
 extern "C"
 {
+void detectAndDraw( Mat& img, CascadeClassifier* cascade,
+                    CascadeClassifier* nestedCascade,
+                    double scale, bool tryflip );
+
 void JNICALL Java_net_monatis_apps_ocvstarter_MainActivity_salt(JNIEnv *env, jobject instance,
                                                                 jlong matAddrGray,
                                                                 jint nbrElem) {
@@ -19,4 +30,113 @@ void JNICALL Java_net_monatis_apps_ocvstarter_MainActivity_salt(JNIEnv *env, job
         mGr.at<uchar>(j, i) = 255;
     }
 }
+
+void JNICALL Java_net_monatis_apps_ocvstarter_MainActivity_setCascadePaths(JNIEnv *env, jobject instance, jstring jMainCascadeName, jstring jNestedCascadeName)
+{
+    const char* cstr1 = env->GetStringUTFChars(jMainCascadeName,NULL);
+    const char* cstr2 = env->GetStringUTFChars(jNestedCascadeName,NULL);
+    std::string mainCascadeName(cstr1);
+    std::string nestedCascadeName(cstr2);
+    mainClassifier.load(mainCascadeName);
+    nestedClassifier.load(nestedCascadeName);
+}
+
+void JNICALL Java_net_monatis_apps_ocvstarter_MainActivity_detectEyes(JNIEnv *env, jobject instance, Mat* frame)
+{
+    Mat& image = *(Mat *) frame;
+    detectAndDraw(image, &mainClassifier, &nestedClassifier, 1.3, false);
+}
+
+void detectAndDraw( Mat& img, CascadeClassifier* cascade,
+                    CascadeClassifier* nestedCascade,
+                    double scale, bool tryflip )
+{
+    double t = 0;
+    std::vector<Rect> faces, faces2;
+    const static Scalar colors[] =
+            {
+                    Scalar(255,0,0),
+                    Scalar(255,128,0),
+                    Scalar(255,255,0),
+                    Scalar(0,255,0),
+                    Scalar(0,128,255),
+                    Scalar(0,255,255),
+                    Scalar(0,0,255),
+                    Scalar(255,0,255)
+            };
+    Mat gray, smallImg;
+
+    cvtColor( img, gray, COLOR_RGBA2GRAY );
+    double fx = 1 / scale;
+    resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR );
+    equalizeHist( smallImg, smallImg );
+
+    t = (double)getTickCount();
+    cascade->detectMultiScale( smallImg, faces,
+                              1.1, 2, 0
+                                      //|CASCADE_FIND_BIGGEST_OBJECT
+                                      //|CASCADE_DO_ROUGH_SEARCH
+                                      |CASCADE_SCALE_IMAGE,
+                              Size(30, 30) );
+    if( tryflip )
+    {
+        flip(smallImg, smallImg, 1);
+        cascade->detectMultiScale( smallImg, faces2,
+                                  1.1, 2, 0
+                                          //|CASCADE_FIND_BIGGEST_OBJECT
+                                          //|CASCADE_DO_ROUGH_SEARCH
+                                          |CASCADE_SCALE_IMAGE,
+                                  Size(30, 30) );
+        for( vector<Rect>::const_iterator r = faces2.begin(); r != faces2.end(); ++r )
+        {
+            faces.push_back(Rect(smallImg.cols - r->x - r->width, r->y, r->width, r->height));
+        }
+    }
+    t = (double)getTickCount() - t;
+    //printf( "detection time = %g ms\n", t*1000/getTickFrequency());
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Rect r = faces[i];
+        Mat smallImgROI;
+        vector<Rect> nestedObjects;
+        Point center;
+        Scalar color = colors[i%8];
+        int radius;
+
+        double aspect_ratio = (double)r.width/r.height;
+        if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
+        {
+            center.x = cvRound((r.x + r.width*0.5)*scale);
+            center.y = cvRound((r.y + r.height*0.5)*scale);
+            radius = cvRound((r.width + r.height)*0.25*scale);
+            circle( img, center, radius, color, 3, 8, 0 );
+        }
+        else
+            rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
+                       cvPoint(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale)),
+                       color, 3, 8, 0);
+        if( nestedCascade->empty() )
+            continue;
+        smallImgROI = smallImg( r );
+        nestedCascade->detectMultiScale( smallImgROI, nestedObjects,
+                                        1.1, 2, 0
+                                                //|CASCADE_FIND_BIGGEST_OBJECT
+                                                //|CASCADE_DO_ROUGH_SEARCH
+                                                //|CASCADE_DO_CANNY_PRUNING
+                                                |CASCADE_SCALE_IMAGE,
+                                        Size(30, 30) );
+        for ( size_t j = 0; j < nestedObjects.size(); j++ )
+        {
+            Rect nr = nestedObjects[j];
+            center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale);
+            center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale);
+            radius = cvRound((nr.width + nr.height)*0.25*scale);
+            circle( img, center, radius, color, 3, 8, 0 );
+        }
+    }
+    //imshow( "result", img );
+}
+
+
+
 }
--- app/src/main/java/net/monatis/apps/ocvstarter/MainActivity.java
+++ app/src/main/java/net/monatis/apps/ocvstarter/MainActivity.java
@@ -1,5 +1,7 @@
 package net.monatis.apps.ocvstarter;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.Manifest;
@@ -10,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import net.monatis.apps.ocvstarter.R;
@@ -20,9 +23,20 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "OCVSample::Activity";
+    String datapath = "";
+    String mainCascadeName = "haarcascade_frontalface_default.xml";
+    String nestedCascadeName = "haarcascade_eye_tree_eyeglasses.xml";
+
     private CameraBridgeViewBase _cameraBridgeViewBase;
 
     private BaseLoaderCallback _baseLoaderCallback = new BaseLoaderCallback(this) {
@@ -34,6 +48,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     // Load ndk built module, as specified in moduleName in build.gradle
                     // after opencv initialization
                     System.loadLibrary("native-lib");
+                    String mainPath = datapath + "/cascades/" + mainCascadeName;
+                    String nestedPath = datapath + "/cascades/" + nestedCascadeName;
+                    setCascadePaths(mainPath, nestedPath);
+                    TextView mainText = (TextView) findViewById(R.id.text_main);
+                    mainText.setText("Shop on mobile with augmented reality");
                     _cameraBridgeViewBase.enableView();
                 }
                 break;
@@ -51,12 +70,16 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
+        datapath = getApplicationContext().getExternalFilesDir(null).getAbsolutePath();
+        checkFiles(new File(datapath + "/cascades/"));
+
         // Permissions for Android 6+
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.CAMERA},
                 1);
 
         _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
+        _cameraBridgeViewBase.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_FRONT);
         _cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         _cameraBridgeViewBase.setCvCameraViewListener(this);
     }
@@ -115,14 +138,79 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     }
 
     public void onCameraViewStopped() {
+        Toast.makeText(getApplicationContext(),"Camera view stopped",Toast.LENGTH_SHORT).show();
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat matGray = inputFrame.gray();
-        salt(matGray.getNativeObjAddr(), 2000);
-        return matGray;
+        Mat matRgba = inputFrame.rgba();
+        //Mat matGray = inputFrame.gray();
+        //salt(matGray.getNativeObjAddr(), 2000);
+        //return matGray;
+        detectEyes(matRgba.getNativeObjAddr());
+        return matRgba;
     }
 
+    private void checkFiles(File dir) {
+        if (!dir.exists()&& dir.mkdirs()){
+            copyFile(mainCascadeName);
+            copyFile(nestedCascadeName);
+        }
+
+        if(dir.exists()) {
+            File datafile;
+            String mainCascadeFilepath = datapath + "/cascades/" + mainCascadeName;
+            datafile = new File(mainCascadeFilepath);
+
+            if (!datafile.exists()) {
+                copyFile(mainCascadeName);
+                Toast.makeText(getApplicationContext(),"Copying main cascade",Toast.LENGTH_SHORT).show();
+            }
+
+            String nestedCascadeFilepath = datapath + "/cascades/" + nestedCascadeName;
+            datafile = new File(nestedCascadeFilepath);
+
+            if (!datafile.exists()) {
+                copyFile(nestedCascadeName);
+                Toast.makeText(getApplicationContext(), "Copying nested cascade", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void copyFile(String filename) {
+        try {
+            String filepath = datapath + "/cascades/" + filename;
+            AssetManager assetManager = getAssets();
+
+            InputStream instream = assetManager.open("cascades/" + filename);
+            OutputStream outstream = new FileOutputStream(filepath);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = instream.read(buffer)) != -1) {
+                outstream.write(buffer, 0, read);
+            }
+
+
+            outstream.flush();
+            outstream.close();
+            instream.close();
+
+            File file = new File(filepath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public native void salt(long matAddrGray, int nbrElem);
+    public native void setCascadePaths(String jMainCascadePath, String jNestedCascadePath);
+    public native void detectEyes(long frame);
+
+
 }
 
