8ef4868d354fddabca0193f1c43d0e021e413019
monatis <yusufsarigoz@gmail.com>
2018-02-19 17:01:16 +0400

    Make it a more generalizable sample project

--- app/src/main/java/net/monatis/apps/ocvstarter/MainActivity.java
+++ app/src/main/java/net/monatis/apps/ocvstarter/MainActivity.java
@@ -2,6 +2,8 @@ package net.monatis.apps.ocvstarter;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 
 import android.Manifest;
@@ -22,6 +24,7 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 
 import java.io.File;
@@ -30,6 +33,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -52,7 +59,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     System.loadLibrary("native-lib");
                     String mainPath = datapath + "/cascades/" + mainCascadeName;
                     String nestedPath = datapath + "/cascades/" + nestedCascadeName;
-                    setCascadePaths(mainPath, nestedPath);
                     TextView mainText = (TextView) findViewById(R.id.text_main);
                     mainText.setText("Shop on mobile with augmented reality");
                     _cameraBridgeViewBase.enableFpsMeter();
@@ -73,8 +79,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
-        datapath = getApplicationContext().getExternalFilesDir(null).getAbsolutePath();
-        checkFiles(new File(datapath + "/cascades/"));
 
         // Permissions for Android 6+
         ActivityCompat.requestPermissions(MainActivity.this,
@@ -146,74 +150,122 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat matRgba = inputFrame.rgba();
-        //Mat matGray = inputFrame.gray();
-        //salt(matGray.getNativeObjAddr(), 2000);
-        //return matGray;
-        detectEyes(matRgba.getNativeObjAddr());
-        return matRgba;
-    }
-
-    private void checkFiles(File dir) {
-        if (!dir.exists()&& dir.mkdirs()){
-            copyFile(mainCascadeName);
-            copyFile(nestedCascadeName);
-        }
-
-        if(dir.exists()) {
-            File datafile;
-            String mainCascadeFilepath = datapath + "/cascades/" + mainCascadeName;
-            datafile = new File(mainCascadeFilepath);
-
-            if (!datafile.exists()) {
-                copyFile(mainCascadeName);
-                Toast.makeText(getApplicationContext(),"Copying main cascade",Toast.LENGTH_SHORT).show();
-            }
-
-            String nestedCascadeFilepath = datapath + "/cascades/" + nestedCascadeName;
-            datafile = new File(nestedCascadeFilepath);
-
-            if (!datafile.exists()) {
-                copyFile(nestedCascadeName);
-                Toast.makeText(getApplicationContext(), "Copying nested cascade", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void copyFile(String filename) {
-        try {
-            String filepath = datapath + "/cascades/" + filename;
-            AssetManager assetManager = getAssets();
-
-            InputStream instream = assetManager.open("cascades/" + filename);
-            OutputStream outstream = new FileOutputStream(filepath);
-
-            byte[] buffer = new byte[1024];
-            int read;
-            while ((read = instream.read(buffer)) != -1) {
-                outstream.write(buffer, 0, read);
-            }
-
-
-            outstream.flush();
-            outstream.close();
-            instream.close();
-
-            File file = new File(filepath);
-            if (!file.exists()) {
-                throw new FileNotFoundException();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Bitmap bitmap = null;
+		Utils.matToBitmap(matRgba, bitmap);
+		Bitmap scannedBitmap = getScanned(bitmap);
+		Mat scannedMat = null;
+		Utils.bitmapToMat(scannedBitmap, scannedMat);
+		return scannedMat;
     }
 
 
-    public native void salt(long matAddrGray, int nbrElem);
-    public native void setCascadePaths(String jMainCascadePath, String jNestedCascadePath);
-    public native void detectEyes(long frame);
+    private Bitmap getScanned(Bitmap original) {
+        Map<Integer, PointF> pointFs = getEdgePoints(original);
+
+        if (isScanPointsValid(pointFs)) {
+            float x1 = pointFs.get(0).x;
+            float x2 = pointFs.get(1).x;
+            float x3 = pointFs.get(2).x;
+            float x4 = pointFs.get(3).x;
+            float y1 = pointFs.get(0).y;
+            float y2 = pointFs.get(1).y;
+            float y3 = pointFs.get(2).y;
+            float y4 = pointFs.get(3).y;
+            Log.d("", "Points(" + x1 + "," + y1 + ")(" + x2 + "," + y2 + ")(" + x3 + "," + y3 + ")(" + x4 + "," + y4 + ")");
+            Bitmap _bitmap = getScannedBitmap(original, x1, y1, x2, y2, x3, y3, x4, y4);
+            return _bitmap;
+        } else {
+            Log.d("", "Invalid scan points");
+            return original;
+        }
+    }
+
+    private Map<Integer, PointF> getEdgePoints(Bitmap tempBitmap) {
+        List<PointF> pointFs = getContourEdgePoints(tempBitmap);
+        Map<Integer, PointF> orderedPoints = orderedValidEdgePoints(tempBitmap, pointFs);
+        return orderedPoints;
+    }
+
+    private Map<Integer, PointF> orderedValidEdgePoints(Bitmap tempBitmap, List<PointF> pointFs) {
+        Map<Integer, PointF> orderedPoints = getOrderedPoints(pointFs);
+        if (!isValidShape(orderedPoints)) {
+            orderedPoints = getOutlinePoints(tempBitmap);
+        }
+        return orderedPoints;
+    }
+
+    public boolean isValidShape(Map<Integer, PointF> pointFMap) {
+        return pointFMap.size() == 4;
+    }
+
+    private Map<Integer, PointF> getOutlinePoints(Bitmap tempBitmap) {
+        Map<Integer, PointF> outlinePoints = new HashMap<>();
+        outlinePoints.put(0, new PointF(0, 0));
+        outlinePoints.put(1, new PointF(tempBitmap.getWidth(), 0));
+        outlinePoints.put(2, new PointF(0, tempBitmap.getHeight()));
+        outlinePoints.put(3, new PointF(tempBitmap.getWidth(), tempBitmap.getHeight()));
+        return outlinePoints;
+    }
+
+    public Map<Integer, PointF> getOrderedPoints(List<PointF> points) {
+
+        PointF centerPoint = new PointF();
+        int size = points.size();
+        for (PointF pointF : points) {
+            centerPoint.x += pointF.x / size;
+            centerPoint.y += pointF.y / size;
+        }
+        Map<Integer, PointF> orderedPoints = new HashMap<>();
+        for (PointF pointF : points) {
+            int index = -1;
+            if (pointF.x < centerPoint.x && pointF.y < centerPoint.y) {
+                index = 0;
+            } else if (pointF.x > centerPoint.x && pointF.y < centerPoint.y) {
+                index = 1;
+            } else if (pointF.x < centerPoint.x && pointF.y > centerPoint.y) {
+                index = 2;
+            } else if (pointF.x > centerPoint.x && pointF.y > centerPoint.y) {
+                index = 3;
+            }
+            orderedPoints.put(index, pointF);
+        }
+        return orderedPoints;
+    }
+
+    private boolean isScanPointsValid(Map<Integer, PointF> pointFs) {
+        return pointFs.size() == 4;
+    }
+
+    private List<PointF> getContourEdgePoints(Bitmap tempBitmap) {
+        float[] points = getPoints(tempBitmap);
+        float x1 = points[0];
+        float x2 = points[1];
+        float x3 = points[2];
+        float x4 = points[3];
+
+        float y1 = points[4];
+        float y2 = points[5];
+        float y3 = points[6];
+        float y4 = points[7];
+
+        List<PointF> pointFs = new ArrayList<>();
+        pointFs.add(new PointF(x1, y1));
+        pointFs.add(new PointF(x2, y2));
+        pointFs.add(new PointF(x3, y3));
+        pointFs.add(new PointF(x4, y4));
+        return pointFs;
+    }
 
 
+
+
+    public native Bitmap getScannedBitmap(Bitmap bitmap, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
+
+    public native Bitmap getGrayBitmap(Bitmap bitmap);
+
+    public native Bitmap getMagicColorBitmap(Bitmap bitmap);
+
+    public native Bitmap getBWBitmap(Bitmap bitmap);
+
+    public native float[] getPoints(Bitmap bitmap);
 }
-
